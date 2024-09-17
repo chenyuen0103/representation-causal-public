08c9f1e3fcddc7c63bb4b1e05d1fe9791132de1c
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-09-17 07:51:50 -0500

    check existing exp

--- causalrep_expms/sec2-4-4-2-sentiment/src/run_causal_text.sh
+++ causalrep_expms/sec2-4-4-2-sentiment/src/run_causal_text.sh
@@ -4,20 +4,13 @@
 TIMESTAMP=$(date +%Y%m%d%H%M%S%N)
 
 PYCODE_SWEEP="causaltext"
-#DATASET_SWEEP="kindle imdb imdb_sents"
-#DATASET_SWEEP="imdb imdb_sents"
 DATASET_SWEEP="toxic_comments"
 HDIM_SWEEP="1024"
 LR_SWEEP="1e-2 1e-1"
-#LR_SWEEP="1e-2"
 L2REG_SWEEP="1e-1 1e-2 1e0"
-#L2REG_SWEEP="1e-2"
 MODE_SWEEP="linear"
 ZDIM_SWEEP="5 10 100 500 1000"
 NUMFEA_SWEEP="20 50 100 500"
-
-#ZDIM_SWEEP="5"
-#NUMFEA_SWEEP="20"
 
 CODE_SUFFIX=".py"
 OUT_SUFFIX=".out"
@@ -25,36 +18,40 @@ PRT_SUFFIX=".txt"
 
 RUN_SCRIPT="run_causal_text_base.sh"
 
-for i in {1..3}; do
-    for ZDIMi in ${ZDIM_SWEEP}; do
-        export ZDIM=${ZDIMi}
-        for L2REGi in ${L2REG_SWEEP}; do
-            export L2REG=${L2REGi}
-            for LRi in ${LR_SWEEP}; do
-                export LR=${LRi}
-                for MODEi in ${MODE_SWEEP}; do
-                    export MODE=${MODEi}
-                    for NUMFEAi in ${NUMFEA_SWEEP}; do
-                        export NUMFEA=${NUMFEAi}
-                        for PYCODE_SWEEPi in ${PYCODE_SWEEP}; do
-                            NAME=bash_${PYCODE_SWEEPi}
-                            for DATASETi in ${DATASET_SWEEP}; do
-                                export DATASET=${DATASETi}
-                                for HDIMi in ${HDIM_SWEEP}; do
-                                    export HDIM=${HDIMi}
-                                    export FILENAME=${PYCODE_SWEEPi}${CODE_SUFFIX}
-                                    export OUTNAME=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_${TIMESTAMP}${OUT_SUFFIX}
-                                    export PRTOUT=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_${TIMESTAMP}${PRT_SUFFIX}
+for ZDIMi in ${ZDIM_SWEEP}; do
+    export ZDIM=${ZDIMi}
+    for L2REGi in ${L2REG_SWEEP}; do
+        export L2REG=${L2REGi}
+        for LRi in ${LR_SWEEP}; do
+            export LR=${LRi}
+            for MODEi in ${MODE_SWEEP}; do
+                export MODE=${MODEi}
+                for NUMFEAi in ${NUMFEA_SWEEP}; do
+                    export NUMFEA=${NUMFEAi}
+                    for PYCODE_SWEEPi in ${PYCODE_SWEEP}; do
+                        NAME=bash_${PYCODE_SWEEPi}
+                        for DATASETi in ${DATASET_SWEEP}; do
+                            export DATASET=${DATASETi}
+                            for HDIMi in ${HDIM_SWEEP}; do
+                                export HDIM=${HDIMi}
+                                export FILENAME=${PYCODE_SWEEPi}${CODE_SUFFIX}
 
-                                    # Check if the output file already exists
-                                    EXISTING_FILE=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_*.out
-                                    if ls ${EXISTING_FILE} 1> /dev/null 2>&1; then
-                                        echo "Skipping ${OUTNAME} as it already exists."
-                                    else
-                                        echo ${NAME}
+                                # Count existing files for this parameter combination
+                                PATTERN=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_*.out
+                                COUNT=$(ls ${PATTERN} 2>/dev/null | wc -l)
+
+                                # Run up to 3 trials
+                                if [ $COUNT -lt 3 ]; then
+                                    for ((trial=$COUNT+1; trial<=3; trial++)); do
+                                        export OUTNAME=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_${TIMESTAMP}_trial${trial}${OUT_SUFFIX}
+                                        export PRTOUT=${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi}_${TIMESTAMP}_trial${trial}${PRT_SUFFIX}
+
+                                        echo "Running trial ${trial} for ${OUTNAME}"
                                         bash ${RUN_SCRIPT} > ${OUTNAME} 2>&1
-                                    fi
-                                done
+                                    done
+                                else
+                                    echo "Skipping ${PYCODE_SWEEPi}_data${DATASETi}_HDIM${HDIMi}_MODE${MODEi}_lr${LRi}_L2REG${L2REGi}_ZDIM${ZDIMi}_NUMFEA${NUMFEAi} as 3 trials already exist."
+                                fi
                             done
                         done
                     done
